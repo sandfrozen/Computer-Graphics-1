1bad7b58e5a446d17f7bf7a12d0bd924dbbac8cc
TomekVW10 <tbuslowski@gmail.com>
2017-10-08 18:01:55 +0200

    final

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.canvas = new System.Windows.Forms.Panel();
+            this.lineButton = new System.Windows.Forms.Button();
+            this.circleButton = new System.Windows.Forms.Button();
+            this.rectangleButton = new System.Windows.Forms.Button();
+            this.startXlabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.startYlabel = new System.Windows.Forms.Label();
+            this.endXlabel = new System.Windows.Forms.Label();
+            this.endYlabel = new System.Windows.Forms.Label();
+            this.startXtext = new System.Windows.Forms.TextBox();
+            this.startYtext = new System.Windows.Forms.TextBox();
+            this.endXtext = new System.Windows.Forms.TextBox();
+            this.endYtext = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.positionY = new System.Windows.Forms.Label();
+            this.positionX = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.canvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.canvas.Location = new System.Drawing.Point(12, 12);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(1319, 897);
+            this.canvas.TabIndex = 0;
+            this.canvas.Click += new System.EventHandler(this.panel1_Click);
+            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.canvasMouseMove);
+            this.canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.canvasMouseUp);
+            // 
+            // lineButton
+            // 
+            this.lineButton.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.lineButton.Location = new System.Drawing.Point(1337, 12);
+            this.lineButton.Name = "lineButton";
+            this.lineButton.Size = new System.Drawing.Size(75, 23);
+            this.lineButton.TabIndex = 1;
+            this.lineButton.Text = "Line";
+            this.lineButton.UseVisualStyleBackColor = false;
+            this.lineButton.Click += new System.EventHandler(this.shapeButtonClicked);
+            // 
+            // circleButton
+            // 
+            this.circleButton.Location = new System.Drawing.Point(1337, 41);
+            this.circleButton.Name = "circleButton";
+            this.circleButton.Size = new System.Drawing.Size(75, 23);
+            this.circleButton.TabIndex = 2;
+            this.circleButton.Text = "Circle";
+            this.circleButton.UseVisualStyleBackColor = true;
+            this.circleButton.Click += new System.EventHandler(this.shapeButtonClicked);
+            // 
+            // rectangleButton
+            // 
+            this.rectangleButton.Location = new System.Drawing.Point(1337, 70);
+            this.rectangleButton.Name = "rectangleButton";
+            this.rectangleButton.Size = new System.Drawing.Size(75, 23);
+            this.rectangleButton.TabIndex = 3;
+            this.rectangleButton.Text = "Rectangle";
+            this.rectangleButton.UseVisualStyleBackColor = true;
+            this.rectangleButton.Click += new System.EventHandler(this.shapeButtonClicked);
+            // 
+            // startXlabel
+            // 
+            this.startXlabel.AutoSize = true;
+            this.startXlabel.Location = new System.Drawing.Point(1337, 103);
+            this.startXlabel.Name = "startXlabel";
+            this.startXlabel.Size = new System.Drawing.Size(37, 13);
+            this.startXlabel.TabIndex = 4;
+            this.startXlabel.Text = "start X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(637, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 5;
+            // 
+            // startYlabel
+            // 
+            this.startYlabel.AutoSize = true;
+            this.startYlabel.Location = new System.Drawing.Point(1337, 142);
+            this.startYlabel.Name = "startYlabel";
+            this.startYlabel.Size = new System.Drawing.Size(37, 13);
+            this.startYlabel.TabIndex = 6;
+            this.startYlabel.Text = "start Y";
+            // 
+            // endXlabel
+            // 
+            this.endXlabel.AutoSize = true;
+            this.endXlabel.Location = new System.Drawing.Point(1337, 181);
+            this.endXlabel.Name = "endXlabel";
+            this.endXlabel.Size = new System.Drawing.Size(35, 13);
+            this.endXlabel.TabIndex = 7;
+            this.endXlabel.Text = "end X";
+            // 
+            // endYlabel
+            // 
+            this.endYlabel.AutoSize = true;
+            this.endYlabel.Location = new System.Drawing.Point(1337, 220);
+            this.endYlabel.Name = "endYlabel";
+            this.endYlabel.Size = new System.Drawing.Size(35, 13);
+            this.endYlabel.TabIndex = 8;
+            this.endYlabel.Text = "end Y";
+            // 
+            // startXtext
+            // 
+            this.startXtext.Location = new System.Drawing.Point(1337, 119);
+            this.startXtext.Name = "startXtext";
+            this.startXtext.Size = new System.Drawing.Size(75, 20);
+            this.startXtext.TabIndex = 9;
+            // 
+            // startYtext
+            // 
+            this.startYtext.Location = new System.Drawing.Point(1337, 158);
+            this.startYtext.Name = "startYtext";
+            this.startYtext.Size = new System.Drawing.Size(75, 20);
+            this.startYtext.TabIndex = 10;
+            // 
+            // endXtext
+            // 
+            this.endXtext.Location = new System.Drawing.Point(1337, 197);
+            this.endXtext.Name = "endXtext";
+            this.endXtext.Size = new System.Drawing.Size(75, 20);
+            this.endXtext.TabIndex = 11;
+            // 
+            // endYtext
+            // 
+            this.endYtext.Location = new System.Drawing.Point(1337, 236);
+            this.endYtext.Name = "endYtext";
+            this.endYtext.Size = new System.Drawing.Size(75, 20);
+            this.endYtext.TabIndex = 12;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(1337, 293);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 17;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.MouseUp += new System.Windows.Forms.MouseEventHandler(this.addShape);
+            // 
+            // positionY
+            // 
+            this.positionY.AutoSize = true;
+            this.positionY.Location = new System.Drawing.Point(1337, 896);
+            this.positionY.Name = "positionY";
+            this.positionY.Size = new System.Drawing.Size(26, 13);
+            this.positionY.TabIndex = 18;
+            this.positionY.Text = "Y: 0";
+            // 
+            // positionX
+            // 
+            this.positionX.AutoSize = true;
+            this.positionX.Location = new System.Drawing.Point(1337, 883);
+            this.positionX.Name = "positionX";
+            this.positionX.Size = new System.Drawing.Size(26, 13);
+            this.positionX.TabIndex = 19;
+            this.positionX.Text = "X: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1424, 921);
+            this.Controls.Add(this.positionX);
+            this.Controls.Add(this.positionY);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.endYtext);
+            this.Controls.Add(this.endXtext);
+            this.Controls.Add(this.startYtext);
+            this.Controls.Add(this.startXtext);
+            this.Controls.Add(this.endYlabel);
+            this.Controls.Add(this.endXlabel);
+            this.Controls.Add(this.startYlabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.startXlabel);
+            this.Controls.Add(this.rectangleButton);
+            this.Controls.Add(this.circleButton);
+            this.Controls.Add(this.lineButton);
+            this.Controls.Add(this.canvas);
+            this.DataBindings.Add(new System.Windows.Forms.Binding("Location", global::WindowsFormsApp1.Properties.Settings.Default, "OK", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.Location = global::WindowsFormsApp1.Properties.Settings.Default.OK;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel canvas;
+        private System.Windows.Forms.Button lineButton;
+        private System.Windows.Forms.Button circleButton;
+        private System.Windows.Forms.Button rectangleButton;
+        private System.Windows.Forms.Label startXlabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label startYlabel;
+        private System.Windows.Forms.Label endXlabel;
+        private System.Windows.Forms.Label endYlabel;
+        private System.Windows.Forms.TextBox startXtext;
+        private System.Windows.Forms.TextBox startYtext;
+        private System.Windows.Forms.TextBox endXtext;
+        private System.Windows.Forms.TextBox endYtext;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Label positionY;
+        private System.Windows.Forms.Label positionX;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Color activeButtonColor = Color.LightSteelBlue;
+
+        List<Graphics> graphics = new List<Graphics>();
+
+        Button[] buttons = new Button[3];
+        Button activeButton;
+        public Form1()
+        {
+            InitializeComponent();
+            lineButton.BackColor = activeButtonColor;
+            buttons[0] = lineButton;
+            buttons[1] = circleButton;
+            buttons[2] = rectangleButton;
+            activeButton = buttons[0];
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            var panel = sender as Panel;
+
+
+            
+        }
+
+
+
+        private void lineBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void shapeButtonClicked(object sender, EventArgs e)
+        {
+            activeButton = sender as Button;
+            disableOtherButtons(activeButton);
+            startXtext.Text = "";
+            startYtext.Text = "";
+            endXtext.Text = "";
+            endYtext.Text = "";
+
+            switch (activeButton.Name)
+            {
+                case "lineButton":
+                    endXlabel.Text = "end X";
+                    endYlabel.Text = "end Y";
+                    endXtext.Enabled = true;
+                    endYtext.Enabled = true;
+                    break;
+                case "circleButton":
+                    endXlabel.Text = "radius";
+                    endYtext.Enabled = false;
+                    break;
+                case "rectangleButton":
+                    endXlabel.Text = "width";
+                    endYlabel.Text = "height";
+                    endYtext.Enabled = true;
+                    break;
+                default:
+                    
+                    break;
+            }
+        }
+
+        private void disableOtherButtons(Button sender)
+        {
+            foreach(var b in buttons) {
+                if( b == sender ){
+                    b.BackColor = activeButtonColor;
+                } else
+                {
+                    b.BackColor = Color.LightGray;
+                }   
+            }
+        }
+
+        private void canvasMouseMove(object sender, MouseEventArgs e)
+        {
+            positionX.Text = string.Format("X: {0}", e.Location.X);
+            positionY.Text = string.Format("Y: {0}", e.Location.Y);
+        }
+
+        private void canvasMouseUp(object sender, MouseEventArgs e)
+        {
+            if (startXtext.Focused)
+            {
+                startXtext.Text = string.Format("{0}", e.Location.X);
+                startYtext.Text = string.Format("{0}", e.Location.Y);
+                startXtext.Enabled = false;
+                startXtext.Enabled = true;
+                startYtext.Enabled = false;
+                startYtext.Enabled = true;
+
+                switch (activeButton.Name)
+                {
+                    case "circleButton":
+                        endXtext.Text = "100";
+                        endYtext.Text = "";
+                        endXtext.Enabled = false;
+                        endXtext.Enabled = true;
+                        endYtext.Enabled = false;
+                        break;
+                    case "rectangleButton":
+                        endXtext.Text = "200";
+                        endYtext.Text = "100";
+                        endXtext.Enabled = false;
+                        endXtext.Enabled = true;
+                        endYtext.Enabled = false;
+                        endYtext.Enabled = true;
+                        break;
+                    default:
+                        break;
+                }
+
+            } else if (endXtext.Focused)
+            {
+                switch (activeButton.Name)
+                {
+                    case "lineButton":
+                        endXtext.Text = string.Format("{0}", e.Location.X);
+                        endYtext.Text = string.Format("{0}", e.Location.Y);
+                        endXtext.Enabled = false;
+                        endXtext.Enabled = true;
+                        endYtext.Enabled = false;
+                        endYtext.Enabled = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void addShape(object sender, MouseEventArgs e)
+        {
+            SolidBrush sb = new SolidBrush(Color.Black);
+            Graphics g = canvas.CreateGraphics();
+
+            switch (activeButton.Name)
+            {
+                case "lineButton":
+                    // add line
+                    var x1 = (float)Convert.ToDouble(startXtext.Text);
+                    var y1 = (float)Convert.ToDouble(startYtext.Text);
+                    var w1 = (float)Convert.ToDouble(endXtext.Text);
+                    var h1 = (float)Convert.ToDouble(endYtext.Text);
+                    Pen pen = new Pen(Color.Black);
+                    pen.Width = 5;
+                    g.DrawLine(pen, x1, y1, w1, h1);
+                    break;
+                case "circleButton":
+                    // add circle
+                    var x2 = Convert.ToInt32(startXtext.Text);
+                    var y2 = Convert.ToInt32(startYtext.Text);
+                    var r2 = Convert.ToInt32(endXtext.Text);
+                    x2 = x2 - r2;
+                    y2 = y2 - r2;
+                    g.FillEllipse(sb, x2, y2, r2*2, r2*2);
+                    break;
+                case "rectangleButton":
+                    // add rectangle  
+                    var x3 = (float)Convert.ToDouble(startXtext.Text);
+                    var y3 = (float)Convert.ToDouble(startYtext.Text);
+                    var w3 = (float)Convert.ToDouble(endXtext.Text);
+                    var h3 = (float)Convert.ToDouble(endYtext.Text);
+                    g.FillRectangle(sb, x3, y3, w3, h3);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
